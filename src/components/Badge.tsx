ee38601e39b0073b1a921bfdfb7db66d31f1654e
simar525 <ssimarpreet11@gmail.com>
2024-03-19 01:06:54 +0530

    chat v4

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -47,14 +47,14 @@ export const Badge = (props: Props) => {
       Powered by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://fiverr.com/cloozoai'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> CloozoAI</span>
       </a>
     </span>
   );
